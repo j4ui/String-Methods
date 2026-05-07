91d6a8162f9a43ed8b101ad51929a86522a5aeb3
Aaron Avdeev <underio3298@gmail.com>
2026-05-07 14:06:26 -0400

    damn ts difficult

--- src/StringPS2_1.java
+++ src/StringPS2_1.java
@@ -4,13 +4,17 @@ public class StringPS2_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int width;
+        int middleIndex;
         String str = scanner.nextLine();
+        String text;
 
-        if (str.length()>10 ) {
-            width = 19;
-        } else {
-            width = str.length();
 
+        if (str.length()>10 ) {width = 19; str = str.substring(0, 9);} else {width = str.length(); text = str;}
+        middleIndex = str.length()-1;
+        for (int i = 0; i < middleIndex; i++) {
+            for (int j = 1; j < width; j++) {
+                if ()
+            }
         }
     }
 }
